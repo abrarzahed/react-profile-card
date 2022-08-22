ba3df5c93744c7057715460a0cb1385e10d0180b
abrarzahed <abrarzahed6986@gmail.com>
2022-08-23 03:20:45 +0600

    completed project

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,21 @@
 import Twitter from "./icons/Twitter";
+import Fb from "./icons/Fb";
+import Instagram from "./icons/Instagram";
+import Github from "./icons/Github";
 export default function Footer() {
   return (
     <footer>
       <a href="#">
         <Twitter />
+      </a>
+      <a href="#">
+        <Fb />
+      </a>
+      <a href="#">
+        <Instagram />
+      </a>
+      <a href="#">
+        <Github />
       </a>
     </footer>
   );
--- components/icons/Twitter.jsx
+++ components/icons/Twitter.jsx
@@ -2,11 +2,8 @@ export default function Twitter() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="48"
-      height="48"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="#000"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
